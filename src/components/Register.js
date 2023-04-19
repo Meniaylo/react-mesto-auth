964e87c4cf4d0a9b9eca900e09988cd90ab480a2
yzma <upthere@yandex.ru>
2023-04-19 11:52:15 +0300

    add correct readme

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,12 +10,6 @@ const Register = ({onRegister}) => {
         submitText="Зарегистрироваться"
         submitFunc={onRegister}
       >
-        <div className="sign-page__register-container">
-          <p className="sign-page__register-text">Уже зарегистрированы?</p>
-          <Link to="/sign-in" className="sign-page__register-text link">
-            Войти
-          </Link>
-        </div>
       </SignPage>
   );
 };
